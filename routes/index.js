34b11a44f22818057bdd26bc2cb1d061431cb10c
Mohinesh Tayade <mohinesh.tayade@toro.com>
2020-06-29 21:42:39 +0530

    added new api

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', (req, res) => {
+  res.send({ 'message': 'Welcome to Node world', name: 'Mohinesh' });
 });
 
 module.exports = router;
